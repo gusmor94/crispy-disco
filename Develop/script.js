f41d3af841561e4c3fa13afc845b0739871c6dec
gusmor94 <gusmor94@users.noreply.github.com>
2020-10-03 15:51:07 -0700

    added prompts to generate random password

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,61 @@
 // Assignment code here
+var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numChar = "1234567890";
+var specialChar = "!@#$&?";
+var characters = ""; 
+
+function generatePassword() {
+  
+  // length of the password
+  var passwordLength = window.prompt("Set the length of your password! Type a number between 8 and 128.");
+  if (8 > passwordLength || passwordLength > 128 || passwordLength === "" || passwordLength === null) {
+    window.alert("Invalid entry.");
+    return generatePassword();
+  }
+  console.log(passwordLength);
+
+  // lowercase characters
+  var lowercase = window.confirm("Would you like lowercase characters in your password?");
+  if (lowercase) {
+    characters = characters + lowerChar;
+  }
+  else {};
+  console.log(lowercase);
+
+  // uppercase characters
+  var uppercase = window.confirm("Would you like uppercase characters in your password?");
+  if (uppercase) {
+    characters = characters + upperChar;
+  }
+  else {};
+  console.log(uppercase);
+  
+  // numeric characters
+  var numbers = window.confirm("Would you like numeric characters in your password?");
+  if(numbers) {
+    characters = characters + numChar;
+  }
+  else {};
+  console.log(numbers);
+
+  // special characters 
+  var symbols = window.confirm("Would you like special characters in your password?");
+  if(symbols) {
+    characters = characters + specialChar;
+  }
+  else {};
+  console.log(symbols);
+
+  // generated password
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomPass = Math.floor(Math.random() * characters.length);
+    password = password + characters[randomPass];
+  }
+  console.log(password);
+  return password;
+}
 
 
 // Get references to the #generate element
